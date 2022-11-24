d14e9a1eb941fa6d86f07ea73bdde10e3f3f967e
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2022-11-24 19:23:12 +0200

    Ilmoittautumisen testailuja

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { Platform, Text, View, Pressable } from "react-native";
 import { style } from "./styles"; 
 import SplashScreen from "./components/SplashScreen";
+import Enrolment from "./components/Enrolment";
 
 export default function App() {
   return (
     <View style={style.StyleSheet}>
       <View>
-        <SplashScreen></SplashScreen>
+        <Enrolment></Enrolment>
       </View>
         <Text h1 style={style.h1Style}>
           Heading 1
--- components/Enrolment.js
+++ components/Enrolment.js
@@ -1,6 +1,48 @@
+import React, { useState }  from 'react';
+import { FlatList, View, SafeAreaView } from "react-native";
+import { Searchbar } from 'react-native-paper';
+import DropDown from "react-native-paper-dropdown";
+
 
 export default function Enrolment() {
   
+    // Searchbar-testailua
+    const [searchQuery, setSearchQuery] = React.useState('');
+  
+    const onChangeSearch = query => setSearchQuery(query);
+  
+    const [items, setItems] = useState([]);
+    const [selectedId, setSelectedId] = useState(null);
+
+    const select = (id) => {
+        setSelectedId(id);
+    }
+
+    const [search, setSearch] = useState('');
+
+
+    return (
+        <View>
+            <FlatList
+            data=""
+            keyExtractor={(item) => item.id}
+            extraData={selectedId}
+            renderItem = {({item}) => (
+                <Row person={item} selectedId={selectedId} select={select}></Row>
+            )}></FlatList>
+        
+
+            <Searchbar
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+            />
+
+            
+        </View>
+    );
+
 
     
 };
+
