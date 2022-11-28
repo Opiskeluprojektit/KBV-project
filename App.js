6fa792f7beeb2077975c0bb41c739be1c7d7351b
jessepesse <58948883+jessepesse@users.noreply.github.com>
2022-11-28 12:17:52 +0200

    Bugi korjattu

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
-import { Platform, Text, View, Pressable} from "react-native";
-import { style } from "./styles/styles.js"; 
-import SplashScreen from "./components/SplashScreen";
 import Home from "./components/Home.jsx";
+import Menu from "./components/Menu.jsx"
 import Points from "./components/Points.jsx";
 import Enrolment from "./components/Enrolment.jsx"
 import Ranking from "./components/Ranking.jsx";
+import CustomMenu from "./components/CustomMenu.jsx";
 import { Provider as PaperProvider } from 'react-native-paper';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
+
+function Root() {
+  return (
+    <Drawer.Navigator drawerContent={props => <Menu {...props} options={{ headerShown: false }}/>} >
+      <Drawer.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Drawer.Screen name="Enrolment" component={Enrolment} options={{ headerShown: false }}/>
+      <Drawer.Screen name="Points" component={Points} options={{ headerShown: false }}/>
+      <Drawer.Screen name="Ranking" component={Ranking} options={{ headerShown: false }}/>
+    </Drawer.Navigator>
+  );
+}
 
 function App() {
   return (
-    
+  
     <NavigationContainer>
     <Stack.Navigator>
+      <Stack.Screen name="Root" component={Root} options={{ headerShown: false }}/>
       <Stack.Screen
         name="Home"
         component={Home}
         options={{ headerShown: false }}
       />
+      <Stack.Screen name="CustomMenu" component={CustomMenu} options={{ headerShown: false }}/>
       <Stack.Screen name="Enrolment" component={Enrolment} options={{ headerShown: false }}/>
       <Stack.Screen name="Points" component={Points} options={{ headerShown: false }}/>
       <Stack.Screen name="Ranking" component={Ranking} options={{ headerShown: false }}/>
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,7 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { DrawerContentScrollView,DrawerItemList } from '@react-navigation/drawer'
 
+const Menu = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  )
+}
 
-export default function Menu() {
-  
-
-    
-};
+export default Menu
